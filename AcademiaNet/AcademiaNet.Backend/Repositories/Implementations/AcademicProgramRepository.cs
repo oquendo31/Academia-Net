6372d69908866a5e376e2bfa969c03a91870b934
juan felipe oquendo lopez <felipeoquendo@hotmail.com>
2025-02-23 22:34:35 -0500

    Revert "prueba 3"
    This reverts commit b6286e567444dd71a155f86686f79c770ae053bf.

--- AcademiaNet/AcademiaNet.Backend/Repositories/Implementations/AcademicProgramRepository.cs
+++ AcademiaNet/AcademiaNet.Backend/Repositories/Implementations/AcademicProgramRepository.cs
@@ -34,7 +34,7 @@ public class AcademicProgramRepository : GenericRepository<AcademicProgram>, IAc
     }
 
     /// <summary>
-    /// prueba commit Juan felipe 15406719
+    /// prueba commit Juan felipe
     /// </summary>
     /// <param name="id"></param>
     /// <returns></returns>
